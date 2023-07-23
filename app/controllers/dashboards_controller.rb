f158b2e2cb56fd43f709998cd335feb457e7cd3e
Ratnaraj Sukale <rvsukale@gmail.com>
2023-07-23 22:05:36 +0530

    added logout button,made Nav Dynamic and fixed Turbo-Button_to issue

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
 
   def show
     if session[:user_id]
-      @user = User.find_by(session[:user_id])
+      @user = User.find_by(id: session[:user_id])
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
