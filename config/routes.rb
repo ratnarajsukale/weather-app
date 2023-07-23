9a21a1a8e87872fdd5263b54f279edde53fe4c24
Ratnaraj Sukale <rvsukale@gmail.com>
2023-07-23 18:30:04 +0530

    added sign_up functionality with validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
